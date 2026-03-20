1ecbd4ebf406998c770308012fcd9579ea67095e
howudong <tjdvy963@naver.com>
2026-03-20 20:31:17 +0900

    feat. api, buildSrc, domain에서 gradle.kts에 필요한 의존성 주입

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.25")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.9.25")
+    implementation("org.jetbrains.kotlin:kotlin-noarg:1.9.25")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.5.11")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
     implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:6.0.1.5171")
