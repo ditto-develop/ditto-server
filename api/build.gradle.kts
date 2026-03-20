acc8f8f42c0892c0f997fc4d27f5c21d60c02f94
howudong <tjdvy963@naver.com>
2026-03-20 20:31:17 +0900

    feat. api, buildSrc, domain에서 gradle.kts에 필요한 의존성 주입

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,14 +6,21 @@ dependencies {
     implementation(project(":common"))
     implementation(project(":domain"))
     implementation(project(":infrastructure"))
+
     implementation(Dependencies.SPRING_BOOT_STARTER_WEB)
     implementation(Dependencies.SPRING_BOOT_STARTER_VALIDATION)
     implementation(Dependencies.SPRING_BOOT_STARTER_SECURITY)
     implementation(Dependencies.SPRING_BOOT_STARTER_AOP)
     implementation(Dependencies.SPRING_BOOT_STARTER_ACTUATOR)
-    runtimeOnly(Dependencies.MICROMETER_PROMETHEUS)
     implementation(Dependencies.LOGSTASH_LOGBACK)
     implementation(Dependencies.SPRINGDOC_OPENAPI)
+    implementation(Dependencies.JJWT_API)
+
+    runtimeOnly(Dependencies.MICROMETER_PROMETHEUS)
+    runtimeOnly(Dependencies.JJWT_IMPL)
+    runtimeOnly(Dependencies.JJWT_JACKSON)
+
+    testImplementation(testFixtures(project(":domain")))
     testImplementation(Dependencies.KOTEST_SPRING)
     testRuntimeOnly(Dependencies.H2)
 }
