c3383cf48977a698368b3ff3e0c718182cc9081f
tuna <tuna@woowahan.com>
2026-03-24 01:07:44 +0900

    refactor. 타임아웃 설정 yml로 분리

--- infrastructure/src/main/kotlin/com/ditto/infrastructure/oauth/kakao/KakaoOAuthProperties.kt
+++ infrastructure/src/main/kotlin/com/ditto/infrastructure/oauth/kakao/KakaoOAuthProperties.kt
@@ -1,10 +1,13 @@
 package com.ditto.infrastructure.oauth.kakao
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import java.time.Duration
 
 @ConfigurationProperties(prefix = "ditto.oauth.kakao")
 data class KakaoOAuthProperties(
     val clientId: String,
     val clientSecret: String,
     val redirectUri: String,
+    val connectTimeout: Duration = Duration.ofSeconds(3),
+    val readTimeout: Duration = Duration.ofSeconds(5),
 )
