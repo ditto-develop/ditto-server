e5ffecbc2616ee759e0a68b4ffbea9b91e112307
howudong <tjdvy963@naver.com>
2026-03-24 23:51:49 +0900

    test. 직접 사용 방식이 아닌, config에서 가져오는 방식으로 대체

--- infrastructure/src/test/kotlin/com/ditto/infrastructure/oauth/kakao/KakaoOAuthClientTest.kt
+++ infrastructure/src/test/kotlin/com/ditto/infrastructure/oauth/kakao/KakaoOAuthClientTest.kt
@@ -1,27 +1,25 @@
 package com.ditto.infrastructure.oauth.kakao
 
+import com.ditto.domain.socialaccount.entity.SocialProvider
+import com.ditto.infrastructure.oauth.OAuthClientFactory
 import com.ditto.infrastructure.oauth.constants.OAuthConstants
-import io.kotest.core.spec.style.FreeSpec
+import com.ditto.infrastructure.support.IntegrationTest
 import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.string.shouldNotContain
 
-class KakaoOAuthClientTest : FreeSpec(
+class KakaoOAuthClientTest(
+    private val properties: KakaoOAuthProperties,
+    private val oAuthClientFactory: OAuthClientFactory,
+) : IntegrationTest(
     {
-        val properties = KakaoOAuthProperties(
-            clientId = "test-client-id",
-            clientSecret = "test-secret",
-            redirectUri = "http://localhost:8080/users/social-login/kakao/callback",
-        )
-
-        val client = KakaoOAuthFakeClient(properties)
-
+        val client = oAuthClientFactory.getClient(SocialProvider.KAKAO)
+        
         "getAuthorizationUrl" - {
             "카카오 인가 URL을 생성한다" {
                 val url = client.getAuthorizationUrl()
 
-                url shouldContain "http://localhost:8080/users/social-login/kakao/callback"
-                url shouldContain "${OAuthConstants.PARAM_CLIENT_ID}=test-client-id"
-                url shouldContain "${OAuthConstants.PARAM_REDIRECT_URI}=http://localhost:8080/users/social-login/kakao/callback"
+                url shouldContain "${OAuthConstants.PARAM_CLIENT_ID}=${properties.clientId}"
+                url shouldContain "${OAuthConstants.PARAM_REDIRECT_URI}=${properties.redirectUri}"
                 url shouldContain "${OAuthConstants.PARAM_RESPONSE_TYPE}=${OAuthConstants.RESPONSE_TYPE_CODE}"
             }
         }
@@ -41,7 +39,13 @@ class KakaoOAuthClientTest : FreeSpec(
             }
 
             "client_secret이 있으면 인가 URL에는 포함되지 않는다" {
-                val clientWithSecret = KakaoOAuthFakeClient(properties)
+                val clientWithSecret = KakaoOAuthFakeClient(
+                    properties = KakaoOAuthProperties(
+                        clientId = "test-id",
+                        clientSecret = "test-secret",
+                        redirectUri = "http://localhost:8080/callback",
+                    ),
+                )
 
                 val url = clientWithSecret.getAuthorizationUrl()
                 url shouldNotContain "test-secret"
--- infrastructure/src/test/kotlin/com/ditto/infrastructure/support/ApplicationConfig.kt
+++ infrastructure/src/test/kotlin/com/ditto/infrastructure/support/ApplicationConfig.kt
@@ -1,6 +1,11 @@
 package com.ditto.infrastructure.support
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 
-@SpringBootApplication(scanBasePackages = ["com.ditto.infrastructure"])
+@SpringBootApplication(
+    scanBasePackages = ["com.ditto.infrastructure"],
+    exclude = [DataSourceAutoConfiguration::class, HibernateJpaAutoConfiguration::class],
+)
 open class ApplicationConfig
