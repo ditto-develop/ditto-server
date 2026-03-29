b97a718411a62aeb26bce92ecff968c900749f3a
tuna <tuna@woowahan.com>
2026-03-29 22:30:59 +0900

    test. 불필요한 스프링 익스텐션 제거

--- buildSrc/src/main/kotlin/spring-convention.gradle.kts
+++ buildSrc/src/main/kotlin/spring-convention.gradle.kts
@@ -15,4 +15,5 @@ dependencies {
     implementation(Dependencies.JACKSON_MODULE_KOTLIN)
     testImplementation(Dependencies.SPRING_BOOT_STARTER_TEST)
     testImplementation(Dependencies.SPRING_MOCKK)
+    testImplementation(Dependencies.KOTEST_SPRING)
 }
