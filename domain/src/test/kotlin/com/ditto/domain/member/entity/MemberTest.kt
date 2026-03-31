b27afcbae269a4ca9facdd87dff2aa882df8c4ba
howudong <tjdvy963@naver.com>
2026-03-31 12:38:01 +0900

    test. 테스트 커버리지를 위한 테스트 코드 작성

--- domain/src/test/kotlin/com/ditto/domain/member/entity/MemberTest.kt
+++ domain/src/test/kotlin/com/ditto/domain/member/entity/MemberTest.kt
@@ -19,6 +19,26 @@ class MemberTest(
                 member.id shouldNotBe 0L
                 member.nickname shouldBe "테스트유저"
             }
+
+            "기본 상태는 PENDING이다" {
+                val member = memberRepository.save(Member(nickname = "테스트유저"))
+
+                member.status shouldBe MemberStatus.PENDING
+                member.isPending() shouldBe true
+            }
+        }
+
+        "Member 상태 변경" - {
+            "activate() 호출 시 ACTIVE 상태로 변경된다" {
+                val member = memberRepository.save(Member(nickname = "테스트유저"))
+
+                member.activate()
+                memberRepository.save(member)
+
+                val found = memberRepository.findById(member.id).get()
+                found.status shouldBe MemberStatus.ACTIVE
+                found.isPending() shouldBe false
+            }
         }
     },
 )
